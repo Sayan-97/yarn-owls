0a5c4ea260e60e1ea74efb793155bcec7c0411e0
Sayan-97 <sdey4232@gmail.com>
2026-02-27 16:15:28 +0530

    feat: initialized nextjs with tailwindcss

---
+++ app/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from "next";
+import { Instrument_Serif, Raleway } from "next/font/google";
+import Footer from "@/components/footer";
+import Header from "@/components/header";
+import "./globals.css";
+
+const raleway = Raleway({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  style: ["normal"],
+  variable: "--font-raleway",
+});
+
+const instrumentSerif = Instrument_Serif({
+  subsets: ["latin"],
+  weight: ["400"],
+  style: ["italic"],
+  variable: "--font-instrument-serif",
+});
+
+export const metadata: Metadata = {
+  title: "Yarn Owls",
+  description: "",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${raleway.variable} ${instrumentSerif.variable}`}>
+        <Header />
+        <main>{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
+}
